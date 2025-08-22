bf18a3a49a30df10d57d8b8a89b8dcc886e31b32
pascofire <donodegayatri012@gmail.com>
2025-08-22 22:11:09 +0530

    main page created, linked login button

---
+++ app/src/main/java/com/gd/codefury/LoginPage.java
@@ -0,0 +1,65 @@
+package com.gd.codefury;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import androidx.annotation.NonNull;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginPage extends AppCompatActivity {
+
+    private EditText editTextEmailAddress;
+    private EditText editTextPhone;
+    private EditText editTextTextPassword;
+    private Button submitButton;
+    FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login_page);
+
+        editTextEmailAddress = findViewById(R.id.editTextTextEmailAddress);
+        editTextPhone = findViewById(R.id.editTextPhone);
+        editTextTextPassword = findViewById(R.id.editTextTextPassword);
+        submitButton = findViewById(R.id.submit);
+        mAuth = FirebaseAuth.getInstance();
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = editTextEmailAddress.getText().toString();
+                String password = editTextTextPassword.getText().toString();
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginPage.this, task -> {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(LoginPage.this, "signed in!", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(LoginPage.this, "login failed", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (view, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
--- app/src/main/java/com/gd/codefury/MainActivity.java
+++ app/src/main/java/com/gd/codefury/MainActivity.java
@@ -1,5 +1,6 @@
 package com.gd.codefury;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,45 +22,19 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText editTextEmailAddress;
-    private EditText editTextPhone;
-    private EditText editTextTextPassword;
-    private Button submitButton;
-    FirebaseAuth mAuth;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        editTextEmailAddress = findViewById(R.id.editTextTextEmailAddress);
-        editTextPhone = findViewById(R.id.editTextPhone);
-        editTextTextPassword = findViewById(R.id.editTextTextPassword);
-        submitButton = findViewById(R.id.submit);
-        mAuth = FirebaseAuth.getInstance();
-
-        submitButton.setOnClickListener(new View.OnClickListener() {
+        Button myButton = findViewById(R.id.login); // Use your actual button ID
+        myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = editTextEmailAddress.getText().toString();
-                String password = editTextTextPassword.getText().toString();
-                mAuth.signInWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(MainActivity.this, task -> {
-                            if (task.isSuccessful()) {
-                                Toast.makeText(MainActivity.this, "signed in!", Toast.LENGTH_SHORT).show();
-                            } else {
-                                Toast.makeText(MainActivity.this, "login failed", Toast.LENGTH_SHORT).show();
-                            }
-                        });
+                Intent intent = new Intent(MainActivity.this, LoginPage.class);
+                startActivity(intent);
             }
-        });
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (view, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
         });
     }
 }
