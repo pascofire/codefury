95a5f969e088f9416ba89e79d8fac9bc736b13d9
pascofire <donodegayatri012@gmail.com>
2025-08-22 17:39:00 +0530

    initializing project and setting up firebase

---
+++ app/src/main/java/com/gd/codefury/MainActivity.java
@@ -0,0 +1,46 @@
+package com.gd.codefury;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextEmailAddress;
+    private EditText editTextPhone;
+    private EditText editTextTextPassword;
+    private Button submitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        editTextEmailAddress = findViewById(R.id.editTextTextEmailAddress);
+        editTextPhone = findViewById(R.id.editTextPhone);
+        editTextTextPassword = findViewById(R.id.editTextTextPassword);
+        submitButton = findViewById(R.id.submit);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Submit clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
