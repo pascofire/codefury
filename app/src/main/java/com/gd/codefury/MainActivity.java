7a4da96a2acd396e31cd6020756c246c11b4d9ec
pascofire <donodegayatri012@gmail.com>
2025-08-23 00:14:46 +0530

    user page created

--- app/src/main/java/com/gd/codefury/MainActivity.java
+++ app/src/main/java/com/gd/codefury/MainActivity.java
@@ -7,7 +7,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -25,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
         Button myButton = findViewById(R.id.login); // Use your actual button ID
